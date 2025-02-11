e19656fc24d01c23feac82b9b090860f6ee3824c
ranjeetsingh1988 <ranjeetsingh220@gmail.com>
2025-02-11 15:31:39 +0530

    Hug Day Code

---
+++ Sample 7/Script.js
@@ -0,0 +1,4 @@
+document.getElementById('hugButton').addEventListener('click', function() {
+    const greeting = document.getElementById('greeting');
+    greeting.style.display = 'block'; // Show the greeting
+});
